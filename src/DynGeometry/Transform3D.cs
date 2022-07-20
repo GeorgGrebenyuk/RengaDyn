4170f0186c803d5564da7a8e375ae18de70c251e
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-20 11:28:49 +0300

    Update Transform3D.cs
    Начал создание

--- src/DynGeometry/Transform3D.cs
+++ src/DynGeometry/Transform3D.cs
@@ -11,13 +11,15 @@ using Renga;
 
 namespace DynRenga.DynGeometry
 {
-    [dr.IsVisibleInDynamoLibrary(false)]
+    /// <summary>
+    /// Класс для работы с интерфейсом Renga.ITransform3D, матрицу трансформации геометрии объектов 4х4 
+    /// </summary>
     public class Transform3D
     {
         public Renga.ITransform3D tr3d;
-        public Transform3D()
+        public Transform3D(object Transform3D_object)
         {
-            
+            this.tr3d = Transform3D_object as Renga.ITransform3D;
         }
     }
 }
