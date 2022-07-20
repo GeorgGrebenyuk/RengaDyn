feb54728f3c9de3108ec9fd43dd57bcb455e65ca
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-20 18:42:26 +0300

    Create Placement3DCollection.cs

---
+++ src/DynGeometry/Placement3DCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using dr = Autodesk.DesignScript.Runtime;
+using dg = Autodesk.DesignScript.Geometry;
+using Renga;
+
+namespace DynRenga.DynGeometry
+{
+    /// <summary>
+    /// Класс для работы с интерфейсом Renga.IPlacement3DCollection, 
+    /// коллекцией отдельных Renga.IPlacement3D
+    /// </summary>
+    public class Placement3DCollection
+    {
+        public Renga.IPlacement3DCollection coll;
+        /// <summary>
+        /// Инициация класса через интерфейс Renga.IPlacement3DCollection
+        /// </summary>
+        /// <param name="Placement3DCollection_object"></param>
+        public Placement3DCollection (object Placement3DCollection_object)
+        {
+            this.coll = Placement3DCollection_object as Renga.IPlacement3DCollection;
+        }
+        /// <summary>
+        /// Получение числа элементов в составе коллекции
+        /// </summary>
+        /// <returns></returns>
+        public int Count()
+        {
+            return this.coll.Count;
+        }
+        /// <summary>
+        /// Получение отдельного интерфейса Renga.IPlacement3D
+        /// по его внутреннему идентификатору в этой коллекции
+        /// </summary>
+        /// <param name="placement3d_index"></param>
+        /// <returns></returns>
+        public object Get(int placement3d_index)
+        {
+            return this.coll.Get(placement3d_index);
+        }
+        /// <summary>
+        /// Получение списка интерфейсов Renga.IPlacement3D
+        /// </summary>
+        /// <returns></returns>
+        public List<object> GetAll()
+        {
+            List<object> objects = new List<object>();
+            for (int i = 0; i < this.coll.Count; i++)
+            {
+                objects.Add(this.coll.Get(i));
+            }
+            return objects;
+        }
+    }
+}
