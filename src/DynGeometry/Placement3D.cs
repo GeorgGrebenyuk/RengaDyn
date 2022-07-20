a6b1b92e3c27e5b652b7baf3e07337e8a155549b
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-20 11:29:20 +0300

    Update Placement3D.cs
    Add class and methods for Renga.IPlacement3D

--- src/DynGeometry/Placement3D.cs
+++ src/DynGeometry/Placement3D.cs
@@ -11,26 +11,143 @@ using Renga;
 
 namespace DynRenga.DynGeometry
 {
-    [dr.IsVisibleInDynamoLibrary(false)]
+    /// <summary>
+    /// Класс для работы с интерфейсом Renga.IPlacement3D - локальной системой координат 
+    /// в трехмерном пространстве
+    /// </summary>
     public class Placement3D
     {
-        private Placement3D() { }
-        /// <summary>
-        /// Получение свойств позиции объекта в пространстве - точку начала и векторы-базис
-        /// </summary>
-        /// <param name="com_IPlacement3D">COM-объект IPlacement3D</param>
-        /// <returns>Словарь с параметрами</returns>
-        [dr.MultiReturn(new[] { "Origin", "AxisX" , "AxisY", "AxisZ" })]
-        public static Dictionary <string,object> GetInfo (object com_IPlacement3D)
+        public Renga.IPlacement3D pl3d;
+        public Placement3D (object Placement3D_object)
         {
-            IPlacement3D place = com_IPlacement3D as IPlacement3D;
-            return new Dictionary<string, object>
-            {
-                {"Origin", place.Origin },
-                {"AxisX", place.AxisX },
-                {"AxisY", place.AxisY },
-                {"AxisZ", place.AxisZ }
-            };
+            this.pl3d = Placement3D_object as Renga.IPlacement3D;
         }
+        //properties
+        /// <summary>
+        /// Получение точки начала СК
+        /// </summary>
+        /// <returns></returns>
+        public dg.Point Origin()
+        {
+            Renga.Point3D p = this.pl3d.Origin;
+            return dg.Point.ByCoordinates(p.X / 1000.0, p.Y / 1000.0, p.Z / 1000.0);
+        }
+        /// <summary>
+        /// Получение вектора X
+        /// </summary>
+        /// <returns></returns>
+        public dg.Vector AxisX()
+        {
+            Renga.Vector3D vX = this.pl3d.AxisX;
+            return dg.Vector.ByCoordinates(vX.X / 1000.0, vX.Y / 1000.0, vX.Z / 1000.0);
+        }
+        /// <summary>
+        /// Получение вектора Y
+        /// </summary>
+        /// <returns></returns>
+        public dg.Vector AxisY()
+        {
+            Renga.Vector3D vY = this.pl3d.AxisY;
+            return dg.Vector.ByCoordinates(vY.X / 1000.0, vY.Y / 1000.0, vY.Z / 1000.0);
+        }
+        /// <summary>
+        /// Получение вектора Z
+        /// </summary>
+        /// <returns></returns>
+        public dg.Vector AxisZ()
+        {
+            Renga.Vector3D vZ = this.pl3d.AxisZ;
+            return dg.Vector.ByCoordinates(vZ.X / 1000.0, vZ.Y / 1000.0, vZ.Z / 1000.0);
+        }
+        //functions
+        /// <summary>
+        /// Преобразование в представление Dynamo CoordinateSystem
+        /// </summary>
+        /// <returns></returns>
+        public dg.CoordinateSystem ToDynamoCoordinateSystem()
+        {
+            return dg.CoordinateSystem.ByOriginVectors(this.Origin(), this.AxisX(), this.AxisY(), this.AxisZ());
+        }
+        /// <summary>
+        /// Проверка, является ли ортогональной данная СК
+        /// </summary>
+        /// <returns></returns>
+        public bool IsOrthogonal()
+        {
+            return this.pl3d.IsOrthogonal();
+        }
+        /// <summary>
+        /// Проверка, является ли нормальной данная СК
+        /// </summary>
+        /// <returns></returns>
+        public bool IsNormal()
+        {
+            return this.pl3d.IsNormal();
+        }
+        /// <summary>
+        /// Проверка, является ли данная СК левосторонней
+        /// </summary>
+        /// <returns></returns>
+        public bool IsLeft()
+        {
+            return this.pl3d.IsLeft();
+        }
+        /// <summary>
+        /// Получение Renga.ITransform3D из текущей СК в глобальную
+        /// </summary>
+        /// <returns></returns>
+        public object GetTransformFrom()
+        {
+            return this.pl3d.GetTransformFrom();
+        }
+        /// <summary>
+        /// Получение Renga.ITransform3D из глобальной СК в текущую
+        /// </summary>
+        /// <returns></returns>
+        public object GetTransformInto()
+        {
+            return this.pl3d.GetTransformInto();
+        }
+        /// <summary>
+        /// Перемещение на указанный вектор
+        /// </summary>
+        /// <param name="vector_to_moving"></param>
+        public void Move (dg.Vector vector_to_moving)
+        {
+            Renga.Vector3D vec;
+            vec.X = vector_to_moving.X * 1000.0;
+            vec.Y = vector_to_moving.Y * 1000.0;
+            vec.Z = vector_to_moving.Z * 1000.0;
+            this.pl3d.Move(vec);
+        }
+        /// <summary>
+        /// Поворот вокруг указанной оси (вектора)
+        /// </summary>
+        /// <param name="vector_axis_rotate"></param>
+        public void Rotate(dg.Vector vector_axis_rotate)
+        {
+            Renga.Vector3D vec;
+            vec.X = vector_axis_rotate.X * 1000.0;
+            vec.Y = vector_axis_rotate.Y * 1000.0;
+            vec.Z = vector_axis_rotate.Z * 1000.0;
+            this.pl3d.Move(vec);
+        }
+        /// <summary>
+        /// Применение трансформации через интерфейс Renga.ITransform3D
+        /// </summary>
+        /// <param name="transform"></param>
+        public void Transform (DynGeometry.Transform3D transform)
+        {
+            this.pl3d.Transform(transform.tr3d);
+        }
+        /// <summary>
+        /// Получение копии текущей СК
+        /// </summary>
+        /// <returns></returns>
+        public object GetCopy()
+        {
+            return this.pl3d.GetCopy();
+        }
+
     }
 }
