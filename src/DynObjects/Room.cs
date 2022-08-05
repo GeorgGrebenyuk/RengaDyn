2318ebf26306259ddb897be8036115633d715d13
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-08-06 00:10:48 +0300

    Adding Room class

---
+++ src/DynObjects/Room.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using dr = Autodesk.DesignScript.Runtime;
+using dg = Autodesk.DesignScript.Geometry;
+using Renga;
+
+namespace DynRenga.DynObjects
+{
+    /// <summary>
+    /// Класс для работы с интерфейсом Renga.IRoom (комнатой)
+    /// </summary>
+    public class Room
+    {
+        public Renga.IRoom room;
+        /// <summary>
+        /// Инициация класса из объекта модели ModelObject
+        /// </summary>
+        /// <param name="ModelObject_Room"></param>
+        public Room (object ModelObject_Room)
+        {
+            this.room = ModelObject_Room as Renga.IRoom;
+        }
+        /// <summary>
+        /// Получение строкового наименования комнаты
+        /// </summary>
+        public string RoomName => this.room.RoomName;
+        /// <summary>
+        /// Получение строкового номера комнаты
+        /// </summary>
+        public string RoomNumber => this.room.RoomNumber;
+        /// <summary>
+        /// Получение маркера-идентификатора помещения (Point2D)
+        /// </summary>
+        public dg.Point MarkerPosition => dg.Point.ByCoordinates( this.room.MarkerPosition.X, this.room.MarkerPosition.Y, 0);
+    }
+}
